c6b771e218dfcff2da9f01393850e8c57d9a278b
Kamil Snietka <krevelir@gmail.com>
2023-01-07 20:35:01 +0100

    added lvl change button and conditions for STR

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-const total = document.querySelector(".js-attTotal");
-const strength = document.querySelector(".js-attStrength");
-const strengthButton = document.querySelector(".js-arrowStrength");
+let total = document.querySelector(".js-attTotal");
+let strength = document.querySelector(".js-attStrength");
+let strengthButton = document.querySelector(".js-arrowStrength");
+
+let level = 1;
+document.querySelector(".js-level").innerHTML = `${level}`;
+
+const levelButton = document.querySelector(".js-arrowLevel");
+
+levelButton.addEventListener("click", () => {
+    if (level < 50) {
+        level++;
+    } else {
+        level = 50;
+    }
+    document.querySelector(".js-level").innerHTML = `${level}`;
+});
+
 
 strengthButton.addEventListener("click", () => {
-    strength.innerText++;
-    total.innerText--;
+    if (strength.innerText < 38 + level * 2) {
+        strength.innerText++;
+        total.innerText--;
+    } else {
+        strength = 38 + level*2;
+    }
 });
